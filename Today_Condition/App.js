20eee1094832fc8a13e1c2305a4dea6658ad96fc
ging53 <ywkim1523@naver.com>
2025-12-06 18:33:27 +0900

    오류수정

--- Today_Condition/App.js
+++ Today_Condition/App.js
@@ -1,22 +1,12 @@
+// App.js
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-import LoginScreen from "./screens/LoginScreen";
-import HomeScreen from "./screens/HomeScreen";
-
-const Stack = createStackNavigator();
+import MainStack from "./navigation/MainStack";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <MainStack />
     </NavigationContainer>
   );
 }
